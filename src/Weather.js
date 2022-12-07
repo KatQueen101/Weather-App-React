d4fd2662c018da3cbd64f41bbbb8876f15b18907
KatQueen101 <brenae1999@hotmail.com>
2022-12-07 14:20:00 -0500

    Adding axios and react changes

--- src/Weather.js
+++ src/Weather.js
@@ -1,73 +1,71 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    date: "Thursday 12:50",
-    description: "Clear",
-    humidity: 49,
-    wind: 9,
-    imgUrl: "http://openweathermap.org/img/wn/01d@2x.png",
-    temperature: 7,
-  };
+export default function Search() {
+  let [city, setCity] = useState(null);
+  let [results, setResults] = useState(null);
 
-  return (
-    <div className="container">
-      <div className="Weather">
-        <div className="weather-app-wrapper">
-          <form className="search-form">
-            <div className="row">
-              <div className="col-6">
-                <input
-                  type="search"
-                  placeholder="Search for city..."
-                  autofocus="on"
-                  autoComplete="off"
-                  className="form-control shadow-sm"
-                />
+  function showWeather(response) {
+    setResults(
+      <div className="container">
+        <div className="Weather">
+          <div className="weather-app-wrapper">
+            <form className="search-form">
+              <div className="row">
+                <div className="col-6">
+                  <input
+                    type="search"
+                    placeholder="Search for city..."
+                    autofocus="on"
+                    autoComplete="off"
+                    className="form-control shadow-sm"
+                  />
+                </div>
+                <div className="btn-toolbar col-3">
+                  <input
+                    type="submit"
+                    value="🔎"
+                    className="btn btn-secondary shadow-sm"
+                  />
+                  <input
+                    type="button"
+                    value="Current"
+                    className="btn btn-secondary shadow-sm"
+                  />
+                </div>
               </div>
-              <div className="btn-toolbar col-3">
-                <input
-                  type="submit"
-                  value="🔎"
-                  className="btn btn-secondary shadow-sm"
-                />
-                <input
-                  type="button"
-                  value="Current"
-                  className="btn btn-secondary shadow-sm"
-                />
-              </div>
-            </div>
-          </form>
-          <br />
-          <div className="row g-3">
-            <div className="col-6 offset-3">
-              <div className="mx-auto card">
-                <div className="row">
-                  <div className="col-2 card-body">
-                    <img
-                      src={weatherData.imgUrl}
-                      alt={weatherData.description}
-                    />
-                  </div>
-                  <div className="col-10 card-body">
-                    <br />
-                    <h1> {weatherData.city} </h1>
-                    <ul className="today-info">
-                      <li> {weatherData.date} </li>
-                      <li> {weatherData.description} </li>
-                      <li>Humidity: {weatherData.humidity}%</li>
-                      <li>Wind: {weatherData.wind} km/h</li>
-                    </ul>
-                    <div className="number">
-                      <h1>
-                        <strong>{weatherData.temperature}</strong>
-                        <span className="units">
-                          <a href="/">°C</a> |<a href="/">°F</a>
-                        </span>
-                      </h1>
+            </form>
+            <br />
+            <div className="row g-3">
+              <div className="col-6 offset-3">
+                <div className="mx-auto card">
+                  <div className="row">
+                    <div className="col-2 card-body">
+                      <img
+                        src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+                        alt={response.data.weather[0].description}
+                      />
+                    </div>
+                    <div className="col-10 card-body">
+                      <br />
+                      <h1> {response.city} </h1>
+                      <ul className="today-info">
+                        <li> {response.date} </li>
+                        <li> {response.data.weather[0].description} </li>
+                        <li>Humidity: {response.data.main.humidity}%</li>
+                        <li>
+                          Wind: {Math.round(response.data.main.speed)} km/h
+                        </li>
+                      </ul>
+                      <div className="number">
+                        <h1>
+                          <strong>{Math.round(response.data.main.temp)}</strong>
+                          <span className="units">
+                            <a href="/">°C</a> |<a href="/">°F</a>
+                          </span>
+                        </h1>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -76,6 +74,30 @@ export default function Weather() {
           </div>
         </div>
       </div>
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=930a3a9d32117e6afd045c48755b3db9&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="searchBar">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city.."
+          onChange={updateCity}
+        ></input>
+        <input type="submit" value="Search"></input>
+      </form>
+      <p>{results}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import axios from "axios";
 
 import Weather from "./Weather";
 
